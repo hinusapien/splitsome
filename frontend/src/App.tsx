f434f24ffdd457dc66b83f29458c9cf89682daa1
Hieu <h.tran@reply.de>
2021-10-07 18:47:48 +0200

    [6] WIP

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,37 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import './App.scss';
+import { ActualExpensesCosts } from './common/types';
 
-const App: FC = () => <h1 data-testid="app-container">Hello World</h1>;
+const App: FC = () => {
+  const billingSum = 0;
+  const billingSumDividedByParticipantNumber = 0;
+  const firstParticipantExpenses = 0;
+  const secondParticipantExpenses = 0;
+  const nParticipantExpenses = 0;
+  const firstParticipantDebts = 0;
+  const secondParticipantDebts = 0;
+  const nParticipantDebts = 0;
+  const x = 'firstParticipant';
+  const y = 'secondParticipant';
+  const z = 'nParticipant';
+  const participantNames: string[] = [x, y, z];
+  const sumOfAllExpenses: number[] = [firstParticipantExpenses, secondParticipantExpenses, nParticipantExpenses];
+  let participantNumber: number;
+  const actualExpensesCosts: ActualExpensesCosts[] = [];
+
+  function getParticipantNumber() {
+    participantNumber = participantNames.length;
+  }
+
+  function calculateBillingSum() {
+    return sumOfAllExpenses.reduce((a, b) => a + b);
+  }
+
+  function addExpense(labelName: string) {
+    actualExpensesCosts.push({ label: labelName });
+  }
+
+  return <h1 data-testid="app-container">Hello World</h1>;
+};
 
 export default App;
---
+++ frontend/src/common/types.ts
@@ -0,0 +1,32 @@
+export interface ActualExpensesCosts {
+  label: string;
+  participantNames: string[];
+  expensesCosts: number;
+}
+
+/*
+LABELNAME: {
+  paidExpenseParticipant1: 0
+  paidExpenseParticipant2: 0
+  paidExpenseParticipant3: 0
+  actualExpenseParticipant1: 0
+  actualExpenseParticipant2: 0
+  actualExpenseParticipant3: 0
+}
+LABELNAME2: {
+  paidExpenseParticipant1: 0
+  paidExpenseParticipant2: 0
+  paidExpenseParticipant3: 0
+  actualExpenseParticipant1: 0
+  actualExpenseParticipant2: 0
+  actualExpenseParticipant3: 0
+}
+LABELNAME3: {
+  paidExpenseParticipant1: 0
+  paidExpenseParticipant2: 0
+  paidExpenseParticipant3: 0
+  actualExpenseParticipant1: 0
+  actualExpenseParticipant2: 0
+  actualExpenseParticipant3: 0
+}
+*/
