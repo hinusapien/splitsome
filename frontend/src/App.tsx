d41ce54c8e4c2f42997c8f025fffc2c83a12bb25
Hieu <h.tran@reply.de>
2021-10-07 16:54:14 +0200

    [3] add eslint and prettier formatter to project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import './App.scss';
 
-const App: FC = () => ( <h1 data-testid='app-container'>Hello World</h1> )
+const App: FC = () => <h1 data-testid="app-container">Hello World</h1>;
 
 export default App;
